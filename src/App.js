7aec83014349fa4c1435242e0b2009840b05a154
Revy <kulumus.kezzi@gmail.com>
2020-05-06 20:30:30 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {motion} from "framer-motion"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div id="checkboxes">
+                <input type="checkbox" name="rGroup" value="1" id="r1"/>
+                <motion.label drag
+                              dragConstraints={{left: -20, right: 20, top: -20, bottom: 20}}
+                              whileHover={{
+                                  scale: 1.1,
+                                  transition: { duration: 0.7 }
+                              }}
+                              whileTap={{
+                                  scale: 1,
+                                  opacity: 0.5,
+                                  borderRadius: "100%",
+                              }} className="checkbox_circle" htmlFor="r1">
+                    <p className="box_text">FRE</p>
+                </motion.label>
+                <input type="checkbox" name="rGroup" value="2" id="r2"/>
+                <motion.label drag
+                              dragConstraints={{left: -20, right: 20, top: -20, bottom: 20}}
+                              whileHover={{
+                                  scale: 1.1,
+                                  transition: { duration: 0.7 }
+                              }}
+                              whileTap={{
+                                  scale: 1,
+                                  opacity: 0.5,
+                                  borderRadius: "100%",
+                              }} className="checkbox_circle" htmlFor="r2">
+                    <p className="box_text">FRE</p>
+                </motion.label>
+                <input type="checkbox" name="rGroup" value="3" id="r3"/>
+                <motion.label drag
+                              dragConstraints={{left: -20, right: 20, top: -20, bottom: 20}}
+                              whileHover={{
+                                  scale: 1.1,
+                                  transition: { duration: 0.7 }
+                              }}
+                              whileTap={{
+                                  scale: 1,
+                                  opacity: 0.5,
+                                  borderRadius: "100%",
+                              }} className="checkbox_circle" htmlFor="r3">
+                    <p className="box_text">FRE</p>
+                </motion.label>
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
